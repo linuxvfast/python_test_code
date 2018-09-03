3e1c34d34fb098ab001dbde4395a88003a8f6349
soft <2559811312@qq.com>
2018-09-03 13:43:53 +0800

    为显示的点删除黑色轮廓,设置线条颜色

--- matplotlib_more_scatter_calculate_upgrade.py
+++ matplotlib_more_scatter_calculate_upgrade.py
@@ -4,6 +4,9 @@ x_value = list(range(1,1001))
 y_value = [x**2 for x in x_value]
 
 plt.scatter(x_value,y_value,edgecolors='none',s=40) #edgecolors删除黑色轮廓
+# plt.scatter(x_value,y_value,c='red',edgecolors='none',s=40) #edgecolors删除黑色轮廓,并设置线条颜色为红色
+#plt.scatter(x_value,y_value,c=(0,0,0.8),edgecolors='none',s=40)#edgecolors删除黑色轮廓,并设置线条颜色为蓝色(红色，绿色，蓝色),值都是0~1之间的小数，越接近0，颜色越深，越接近1，颜色越浅
+
 plt.title('Square Numbers',fontsize=24)
 plt.xlabel('Value',fontsize=14)
 plt.ylabel('Square of Value',fontsize=14)
