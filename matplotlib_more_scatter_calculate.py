89c7c709b62878b5fd782ac6720becc73dbf5f82
soft <2559811312@qq.com>
2018-09-03 11:59:47 +0800

    大量的散点生成图形

---
+++ matplotlib_more_scatter_calculate.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+x_value = list(range(1,1001))
+y_value = [x**2 for x in x_value]
+
+plt.scatter(x_value,y_value,s=40)
+plt.title('Square Numbers',fontsize=24)
+plt.xlabel('Value',fontsize=14)
+plt.ylabel('Square of Value',fontsize=14)
+plt.axis([0,1100,0,1100000])   #设置x轴和y轴的取值范围
+plt.show()
