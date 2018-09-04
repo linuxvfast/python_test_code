c73af638e1f757dd56ab57c88f207e5321d8283a
soft <2559811312@qq.com>
2018-09-04 09:41:51 +0800

    使用队列简化线程数量可以检查大量的ip地址是否存活避免过多的上下文切换

--- queue_check_host.py
+++ queue_check_host.py
@@ -1,36 +1,49 @@
-import subprocess
 import threading
+import subprocess
 from queue import Queue
 from queue import Empty
 
-def call_ping(ip):
-    if subprocess.call(["ping", "-c", "1", ip]):
-        print("{0} is alive".format(ip))
-    else:
-        print("{0} is unreacheable".format(ip))
+whether = []
 
-def is_reacheable(q):
+def is_alive(ip):
+    '''
+    检查主机是否存货
+    :param ip: 需要检查的ip地址
+    :return: 主机是否存货的结果
+    '''
+    if subprocess.call(["ping","-c","2",ip]):
+        whether.append('{0} is alive'.format(ip))
+    else:
+        whether.append('{0} is down'.format(ip))
+
+def call_whether(q):
     try:
         while True:
-            ip = q.get_nowait()
-            call_ping(ip)
+            ip = q.get_nowait()  #不阻塞
+            is_alive(ip)
     except Empty:
         pass
 
+
 def main():
     q = Queue()
-    with open('ips.txt') as f:
+    with open('ips.txt','r') as f:
         for line in f:
-            q.put(line)
+            q.put(line) #读取的ip放到队列中
 
-    threads = []
-    for i in range(10):
-        thr = threading.Thread(target=is_reacheable, args=(q,))
-        thr.start()
-        threads.append(thr)
+        threads = []
+        for line in range(10):
+            thr = threading.Thread(target=call_whether,args=(q,))
+            thr.start()
+            threads.append(thr)
 
-    for thr in threads:
-        thr.join()
+        #等待所有的线程执行完毕
+        for thr in threads:
+            thr.join()
+            
+        print('========================')
+        for whe in whether:#打印检测结果
+            print(whe)
 
 if __name__ == '__main__':
     main()
